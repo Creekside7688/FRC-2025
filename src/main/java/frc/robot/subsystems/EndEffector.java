6bd166617a863d6cc51fae5bdfd6baba4a0773e9
GrantedPlayer <mr.poggersman1234@gmail.com>
2025-01-21 14:17:18 -0800

    End Effector Proximity Sensor

--- src/main/java/frc/robot/subsystems/EndEffector.java
+++ src/main/java/frc/robot/subsystems/EndEffector.java
@@ -7,18 +7,27 @@ import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.EndEffectorConstants;
 
 public class EndEffector extends SubsystemBase {
   private final SparkMax motor = new SparkMax(EndEffectorConstants.END_EFFECTOR_MOTOR_ID, MotorType.kBrushless);
   
+  private final DigitalInput sensor = new DigitalInput(8);
 
   public EndEffector() {
 
   }
   public void run(double speed) {
     motor.set(speed);
+  }
+
+  public boolean sensordetect() {
+    sensor.get();
+    boolean sensorvalue = sensor.get();
+    return sensorvalue;
   }
   @Override
   public void periodic() {
