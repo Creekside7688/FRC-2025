ebc93841f44177a478b9579eaf140dc83e461f85
oaktrees4dawin <oaktrees4dawin@github.com>
2025-04-13 17:52:56 -0700

    2025-04-13 17:53

--- src/main/java/frc/robot/constants/VisionConstants.java
+++ src/main/java/frc/robot/constants/VisionConstants.java
@@ -21,10 +21,10 @@ public class VisionConstants {
 
         public static final double TOLERANCE = 0.02;
 
-        //public static final double kV_MAX = 4.8;
+        // public static final double kV_MAX = 4.8;
         public static final double kV_MAX = 0.5;
         public static final double kA_MAX = 1.0;
-//        public static final double kA_MAX = 2.0;
+        // public static final double kA_MAX = 2.0;
     }
 
     public static class THETA_CONTROLLER {
@@ -33,12 +33,11 @@ public class VisionConstants {
         public static final double kD = 0;
 
         public static final double TOLERANCE = Units.degreesToRadians(2.0);
-//        public static final double kV_MAX = 2 * Math.PI;
+        // public static final double kV_MAX = 2 * Math.PI;
         public static final double kV_MAX = 0.5 * Math.PI;
         public static final double kA_MAX = 2.75;
-//        public static final double kA_MAX = 5.0;
+        // public static final double kA_MAX = 5.0;
     }
-
 
     public static final double GOAL_X = 2.5;
     public static final double GOAL_Y = 0;
@@ -54,14 +53,13 @@ public class VisionConstants {
     public static final Transform3d ROBOT_TO_CAM = new Transform3d(new Translation3d(ROBOT_CAMERA_OFFSET_FWD,
             ROBOT_CAMERA_OFFSET_SIDE, ROBOT_CAMERA_OFFSET_VERT), new Rotation3d(CAMERA_ROLL, CAMERA_PITCH, CAMERA_YAW));
 
-	public static final double HIGHEST_AMBIGUITY = 0.2;
-	public static final String CAMERA_NAME = "7688Camera";
+    public static final double HIGHEST_AMBIGUITY = 2.0;
+    public static final String CAMERA_NAME = "7688Camera";
 
     public static final double FIELD_LENGTH_METERS = 16.54175;
     public static final double FIELD_WIDTH_METERS = 8.0137;
 
     public static final Pose2d FLIPPING_POSE = new Pose2d(
-        new Translation2d(FIELD_LENGTH_METERS, FIELD_WIDTH_METERS),
-        new Rotation2d(Math.PI)
-    );
+            new Translation2d(FIELD_LENGTH_METERS, FIELD_WIDTH_METERS),
+            new Rotation2d(Math.PI));
 }
